30c51c3f1ed4321fad9aa07b589c6fea684b7160
Deborah Barcenas <deborah.barcenas@gmail.com>
2020-06-29 16:27:16 +0000

    Automatically backed up by Learn

--- db/migrate/20200629162520_create_cats.rb
+++ db/migrate/20200629162520_create_cats.rb
@@ -1,4 +1,8 @@
 class CreateCats < ActiveRecord::Migration
-  def change
+  create_table :cats do |t|
+    t.string :name
+    t.integer :age
+    t.string :breed
+    t.integer :owner_id
   end
 end
